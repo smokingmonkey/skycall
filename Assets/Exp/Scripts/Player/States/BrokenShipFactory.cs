5fc0987eff0201d23c481480690ac95b34df6e61
Andy Esteban Ochoa <seventub@gmail.com>
2023-10-27 06:19:34 -0500

    Update: Some extra assets

---
+++ Assets/Exp/Scripts/Player/States/BrokenShipFactory.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Zenject;
+
+namespace Exp.Scripts.Player.States
+{
+    public class BrokenShipFactory : PlaceholderFactory<Transform>
+    {
+    }
+}
+
+
---
+++ Assets/Exp/Scripts/Player/States/ExplosionFactory.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+namespace Exp.Scripts.Player.States
+{
+    public class ExplosionFactory : PlaceholderFactory<Transform>
+    {
+    }
+}
+
